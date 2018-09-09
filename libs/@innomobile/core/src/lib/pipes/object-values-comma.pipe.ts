79fbdb62356383ca64ae8e99fd2535db72831a74
DESKTOP\paul <paulstelzer@gmail.com>
2018-09-09 18:42:12 +0200

    fix(core): Fix Object Values Comma Pipe

--- libs/@innomobile/core/src/lib/pipes/object-values-comma.pipe.ts
+++ libs/@innomobile/core/src/lib/pipes/object-values-comma.pipe.ts
@@ -3,7 +3,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 @Pipe({ name: 'objectValuesComma' })
 export class ObjectValuesCommaPipe implements PipeTransform {
   transform(value, args: string[]): any {
-    const values = Object.keys(value).map(key => value[key]).map(x => x.substr(0, x.length - 4));
+    const values = Object.keys(value).map(key => value[key]);
     const commaJoinedValues = values.join(',');
     return commaJoinedValues;
   }
